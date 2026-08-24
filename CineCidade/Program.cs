e47b7cff3b478c5625fe219a898b2aabc1ea6b05
labsfiap <labsfiap@PA1005MICRO23>
2026-08-24 20:00:02 -0300

    Add project files.

---
+++ CineCidade/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Nome do cliente: ");
+string nome = Console.ReadLine();
+
+Console.Write("Quantidade de ingressos: ");
+int quantidade = int.Parse(Console.ReadLine());
+
+Console.Write("Cupom de desconto: ");
+string cupom = Console.ReadLine();
+
+Console.Write("Idade do cliente: ");
+int idade = int.Parse(Console.ReadLine());
+
+decimal custoBruto = 5.00m + (25.00m * quantidade);
+
+Console.WriteLine("Valor bruto: " + custoBruto);
+Console.ReadLine();
